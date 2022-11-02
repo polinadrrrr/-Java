a061c7f012e55a0e3e9aa012e18c8824ce5616b5
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2022-11-02 20:25:36 +0700

    Update 4.java

--- Lab-2/4.java
+++ Lab-2/4.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-	    Scanner console = new Scanner(System.in);
-	    System.out.print("Enter h: ");
-	    float h = console.nextFloat();
-	    double res;
-	    res = Math.sqrt(2*h/9.8);
+		Scanner console = new Scanner(System.in);
+		System.out.print("Enter h: ");
+		float h = console.nextFloat();
+		double res;
+		res = Math.sqrt(2*h/9.8);
 		System.out.println("Time: " + res);
 	}
 }
