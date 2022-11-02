7f429c91db49563fdd8f5c11d2a2ae8781c3a6f3
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2022-11-02 11:37:32 +0700

    Update 2.java

--- Lab-3/2.java
+++ Lab-3/2.java
@@ -5,7 +5,7 @@ public class Main
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter m: ");
-        int m = input.nextInt();
+        	int m = input.nextInt();
 		double res = 0, n = 1;
 		for (int i = 1; i <= m; i++) {
 		    if (i%2 == 0) {
