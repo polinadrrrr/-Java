fa5172287f5af1080ceed04874a230aa57d17ed2
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2022-11-02 20:23:28 +0700

    Update 2.java

--- Lab-2/2.java
+++ Lab-2/2.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-	    Scanner console = new Scanner(System.in);
-	    int res = 1;
-	    System.out.print("Enter n: ");
-	    int n = console.nextInt();
-	    for (int i = 1; i <= n; i++) {
-	        res *= i;
-	    }
+		Scanner console = new Scanner(System.in);
+		int res = 1;
+		System.out.print("Enter n: ");
+		int n = console.nextInt();
+		for (int i = 1; i <= n; i++) {
+			res *= i;
+		}
 		System.out.println("n! = " + res);
 	}
 }
