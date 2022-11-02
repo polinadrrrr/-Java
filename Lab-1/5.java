ef618aea336df4b58e1732ddc424a23b1d6bd55c
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2022-11-02 19:44:42 +0700

    Update 5.java

--- Lab-1/5.java
+++ Lab-1/5.java
@@ -1,13 +1,12 @@
 /* Сальцова Полина, 20.09.2022*/
 public class Main
 {
-    public static int a = 1;
-    public static int b = 3;
-    public static int c = 9;
-    public static int d = 27;
-    
+	public static int a = 1;
+	public static int b = 3;
+	public static int c = 9;
+	public static int d = 27;
 	public static void main(String[] args) {
-	    int result = -a + b - c + d;
-	    System.out.println(result);
+		int result = -a + b - c + d;
+		System.out.println(result);
 	}
 }
