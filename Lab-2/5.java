4335f82c6cd8542bc7ea20c5df0765b39b5fa8d0
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2022-11-02 20:26:46 +0700

    Update 5.java

--- Lab-2/5.java
+++ Lab-2/5.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-	    Scanner console = new Scanner(System.in);
-	    System.out.print("Enter c: ");
-	    double c = console.nextFloat();
-	    System.out.print("Enter a: ");
-	    double a = console.nextFloat();
-	    double b;
-	    if (c <= a) {
-	        System.out.println("Error");
-	    }
-	    else {
-	        b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
-	        System.out.println("b = " + b);
-	    }
+		Scanner console = new Scanner(System.in);
+		System.out.print("Enter c: ");
+		double c = console.nextFloat();
+		System.out.print("Enter a: ");
+		double a = console.nextFloat();
+		double b;
+		if (c <= a) {
+			System.out.println("Error");
+		}
+		else {
+			b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
+			System.out.println("b = " + b);
+		}
 	}
 }
