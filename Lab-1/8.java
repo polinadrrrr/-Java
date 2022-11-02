61f7bca4dac44c32272fccd5b8eb4d11dd61c32c
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2022-11-02 20:20:43 +0700

    Update 8.java

--- Lab-1/8.java
+++ Lab-1/8.java
@@ -1,12 +1,12 @@
 /* Сальцова Полина, 20.09.2022*/
 public class Main
 {
-    public static void main(String[] args) {
-//        String name = "Elly";
-//        String text = "Hello " + name + "!";
-//        System.out.println(text);
-        String name = "Amigo";
-        String text = "Hello " + name + "!";
-        System.out.println(text);
+	public static void main(String[] args) {
+		//String name = "Elly";
+		//String text = "Hello " + name + "!";
+		//System.out.println(text);
+		String name = "Amigo";
+		String text = "Hello " + name + "!";
+		System.out.println(text);
 	}
 }
