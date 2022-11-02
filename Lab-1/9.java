4f47902b92a8eff2f1ded05fb3dedf819462079f
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2022-11-02 20:21:24 +0700

    Update 9.java

--- Lab-1/9.java
+++ Lab-1/9.java
@@ -1,10 +1,10 @@
 /* Сальцова Полина, 20.09.2022*/
 public class Main
 {
-    public static void main(String[] args) {
-        String s1 = "Coding";
-        String s2 = "in";
-        String s3 = "pascal";
-        System.out.println(s1 + " " + s2 + " " + s3);
+	public static void main(String[] args) {
+		String s1 = "Coding";
+		String s2 = "in";
+		String s3 = "pascal";
+		System.out.println(s1 + " " + s2 + " " + s3);
 	}
 }
