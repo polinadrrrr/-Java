6933ef6ee945cb8b96c53c515d3f840cbc002333
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2022-11-02 20:19:34 +0700

    Update 7.java

--- Lab-1/7.java
+++ Lab-1/7.java
@@ -1,26 +1,26 @@
 /* Сальцова Полина, 20.09.2022*/
 public class Main
 {
-    public static void main(String[] args) {
-//        String s = "Happy";
-//        System.out.println("New Year");
-//        System.out.println("Happy New");
-//        System.out.println("Year");
-//        System.out.print("Happy New");
-//        System.out.println("Year");
-//        System.out.println(s);
-//        System.out.print("Happy New!");
-//        System.out.print(" ");
-//        System.out.println("Year");
-//        String s = "Happy";
-//        System.out.println("New Year");
-//        System.out.println("Happy New");
-//        System.out.println("Year");
-        System.out.print("Happy New");
-//        System.out.println("Year");
-//        System.out.println(s);
-//        System.out.print("Happy New!");
-        System.out.print(" ");
-        System.out.println("Year");
+	public static void main(String[] args) {
+		//String s = "Happy";
+		//System.out.println("New Year");
+		//System.out.println("Happy New");
+		//System.out.println("Year");
+		//System.out.print("Happy New");
+		//System.out.println("Year");
+		//System.out.println(s);
+		//System.out.print("Happy New!");
+		//System.out.print(" ");
+		//System.out.println("Year");
+		//String s = "Happy";
+		//System.out.println("New Year");
+		//System.out.println("Happy New");
+		//System.out.println("Year");
+		System.out.print("Happy New");
+		//System.out.println("Year");
+		//System.out.println(s);
+		//System.out.print("Happy New!");
+		System.out.print(" ");
+		System.out.println("Year");
 	}
 }
