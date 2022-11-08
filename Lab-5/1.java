e31c37adbd16b3758918339228888077353fa5da
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2022-11-08 10:27:28 +0700

    Update 1.java

--- Lab-5/1.java
+++ Lab-5/1.java
@@ -10,31 +10,30 @@ public class Main
 		int[] array = new int[n];
 		System.out.print("Enter array: ");
 		for (int i = 0; i < n; i++) {
-		    array[i] = input.nextInt();
+			array[i] = input.nextInt();
 		}
 		System.out.print("Array: ");
 		for (int i = 0; i < n; i++) {
-		    System.out.print(array[i] + " ");
+			System.out.print(array[i] + " ");
 		}
 		min = array[0];
 		max = array[0];
 		for (int i = 0; i < n; i++) {
-		    if (array[i] < min) {
-		        min = array[i];
-		        i_min = i;
-		    }
-		    if (array[i] > max) {
-		        max = array[i];
-		        i_max = i;
-		    }
+			if (array[i] < min) {
+				min = array[i];
+				i_min = i;
+			}
+			if (array[i] > max) {
+				max = array[i];
+				i_max = i;
+			}
 		}
 		buf = array[i_min];
 		array[i_min] = array[i_max];
 		array[i_max] = buf;
 		System.out.print("\nResult: ");
 		for (int i = 0; i < n; i++) {
-		    System.out.print(array[i] + " ");
+			System.out.print(array[i] + " ");
 		}
-		
 	}
 }
