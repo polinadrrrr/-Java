2ce1ee6daee1f5a97b327e5c3954b4040b8d05d9
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2022-11-08 10:33:33 +0700

    Update 5.java

--- Lab-5/5.java
+++ Lab-5/5.java
@@ -5,17 +5,13 @@ public class Main
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int[][] array = new int[3][5];
-		for (int i = 0; i < 3; i++) {
-		    for (int j = 0; j < 5; j++) {
-		        array[i][j] = (int) ( Math.random() * 10 );
-		    } 
-		}
 		System.out.print("Array: \n");
 		for (int i = 0; i < 3; i++) {
-		    for (int j = 0; j < 5; j++) {
-		        System.out.print(array[i][j] + " ");
-		    }
-		    System.out.println();
+			for (int j = 0; j < 5; j++) {
+				array[i][j] = (int) ( Math.random() * 10 );
+				System.out.print(array[i][j] + " ");
+			}
+			System.out.println();
 		}
 	}
 }
