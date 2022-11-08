09eca5644ba9d768da0b251914f0c2a7204ae696
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2022-11-08 11:09:25 +0700

    Update 6.java

--- Lab-5/6.java
+++ Lab-5/6.java
@@ -2,39 +2,45 @@
 import java.util.Scanner;
 public class Main
 {
+	public static int Max(int[] inArray) { 
+		int max = inArray[0], index_max = 0;
+		for(int i = 0; i < inArray.length; i++) { 
+			if(inArray[i] > max) { 
+				max = inArray[i];
+				index_max = i;
+			} 
+		}
+		System.out.print("\nMaximum: " + max);
+		return index_max;
+	}
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter n: ");
-		int min, max, i_min = 0, i_max = 0, buf;
+		int min, i_min = 0, i_max = 0, buf;
 		int n = input.nextInt();
 		int[] array = new int[n];
 		System.out.print("Enter array: ");
 		for (int i = 0; i < n; i++) {
-		    array[i] = input.nextInt();
+			array[i] = input.nextInt();
 		}
 		System.out.print("Array: ");
 		for (int i = 0; i < n; i++) {
-		    System.out.print(array[i] + " ");
+			System.out.print(array[i] + " ");
 		}
 		min = array[0];
-		max = array[0];
 		for (int i = 0; i < n; i++) {
-		    if (array[i] < min) {
-		        min = array[i];
-		        i_min = i;
-		    }
-		    if (array[i] > max) {
-		        max = array[i];
-		        i_max = i;
-		    }
+			if (array[i] < min) {
+				min = array[i];
+				i_min = i;
+			}
 		}
+		i_max = Max(array);
 		buf = array[i_min];
 		array[i_min] = array[i_max];
 		array[i_max] = buf;
 		System.out.print("\nResult: ");
 		for (int i = 0; i < n; i++) {
-		    System.out.print(array[i] + " ");
+			System.out.print(array[i] + " ");
 		}
-		
 	}
 }
