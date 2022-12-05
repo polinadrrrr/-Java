e1d400914053888d2115ce205bb2a3e16004e492
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2022-12-05 17:26:57 +0700

    Update 1.java

--- Lab-6/1.java
+++ Lab-6/1.java
@@ -2,14 +2,14 @@
 import java.util.Date;
 public class Main
 {
-    public static void main(String[] args) {
-    	long totalMilliseconds = System.currentTimeMillis();
-    	long totalSeconds = totalMilliseconds / 1000;
-    	long currentSecond = totalSeconds % 60;
-    	long totalMinutes = totalSeconds / 60;
-    	long currentMinute = totalMinutes % 60;
-    	long totalHours = totalMinutes / 60;
-    	long currentHour = totalHours % 24;
-    	System.out.println("Текущее время " + currentHour + ":" + currentMinute + ":" + currentSecond + " GMT");
-    }
+	public static void main(String[] args) {
+		long totalMilliseconds = System.currentTimeMillis();
+		long totalSeconds = totalMilliseconds / 1000;
+		long currentSecond = totalSeconds % 60;
+		long totalMinutes = totalSeconds / 60;
+		long currentMinute = totalMinutes % 60;
+		long totalHours = totalMinutes / 60;
+		long currentHour = totalHours % 24;
+		System.out.println("Текущее время " + currentHour + ":" + currentMinute + ":" + currentSecond + " GMT");
+	}
 }
